e1d2ecb68c69432354989222ea197057995f6390
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-13 14:02:59 -0600

    added a username input box and an updated prompt for Hello (username)

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+// User Acception
+
+// let username;
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Hello ${username}`
+}
+
+
+
 // Button Features
 document.querySelector('.button').addEventListener('click', function() {
     alert("Typing test has been reset!");
