e633fed66b06c78842cea952999b3407bdf882df
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-13 18:26:53 -0600

    made changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 // User Acception
 
+// Add a "create prompt" button.
+    // Prompt will appear on screen for user to type out a new prompt. (Users can create and delete prompts).
+// Create a text box for typing to take place.
+    // User can only type within this text box.
+    // When the user has finished the prompt, the test will automatically stop and tell the user how long it took to complete.
+
+
 let username;
 
 document.getElementById("mySubmit").onclick = function(){
